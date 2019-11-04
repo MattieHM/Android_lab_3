6a6f825ce6ff4fc40e040f17b4ee02c36c7da254
MattieHM <46723400+MattieHM@users.noreply.github.com>
2019-11-04 11:50:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+
+	Паттерны из https://refactoring.guru/ru/design-patterns
+
+					Behavioral Patterns
+	
+	State
+	Chain of Responsibility
+	Visitor
+	
+					Creational Patterns
+	Factory Method
+	Singleton
+	Abstract Factory
+	
+					Structural Patterns
+	Facade
+	Protection Proxy
+	Composite
+
+	
---
+++ patterns/src/test/kotlin/ProtectionProxy.kt
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test
+
+interface File {
+    fun read(name: String)
+}
+
+class NormalFile : File {
+    override fun read(name: String) = println("Reading file: $name")
+}
+
+class SecuredFile(private val normalFile: File) : File {
+    var password: String = ""
+
+    override fun read(name: String) {
+        if (password == "secret") {
+            println("Password is correct: $password")
+            normalFile.read(name)
+        } else {
+            println("Incorrect password. Access denied!")
+        }
+    }
+}
+
+class ProtectionProxyTest {
+    @Test
+    fun `Protection Proxy`() {
+        val securedFile = SecuredFile(NormalFile())
+
+        with(securedFile) {
+            read("readme.md")
+            password = "secret"
+            read("readme.md")
+        }
+    }
+}
